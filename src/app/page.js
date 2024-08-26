3c25056080b13752b5e3f9b2a373de1648cd252a
Mohamed Yehia <eng.essa85@gmail.com>
2024-08-27 00:57:13 +0300

    Adding project files

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,165 @@
 import Image from "next/image";
+import Link from "next/link";
+import Nav from "./components/Nav";
+import { RiFacebookBoxFill } from "react-icons/ri";
+import { BsInstagram } from "react-icons/bs";
+import { FaYoutube } from "react-icons/fa";
+import { BsLinkedin } from "react-icons/bs";
+import { FaTwitter } from "react-icons/fa";
+
+
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <Nav />
+
+      <section className="container mx-auto p-6 flex lg:flex-row flex-col-reverse">
+        <div className="lg:w-1/2 w-full flex flex-col lg:items-start space-y-5 items-center">
+          <h1 className="font-bold lg:text-5xl text-3xl text-center lg:text-start">
+            Rapidly build modern websites without ever leaving your HTML.
+          </h1>
+          <p className="text-center lg:text-start">
+            A utility-first CSS framework packed with classes like flex, pt-4,
+            text-center and rotate-90 that can be composed to build any design,
+            directly in your markup.
+          </p>
+          <button className="button-style">Get Started</button>
         </div>
-      </div>
+        <div className="lg:w-1/2 w-full flex justify-center mb-7 lg:mb-0">
+          <Image
+            src="/undraw_video_games_x1tr.svg"
+            alt=""
+            width={400}
+            height={400}
+          />
+        </div>
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className="container mx-auto p-6 flex flex-col my-8 items-center space-y-8">
+        <h1 className="font-bold text-3xl">
+          “Best practices” don’t actually work.
+        </h1>
+        <p className="text-center">
+          ’ve written a few thousand words on why traditional “semantic class
+          names” are the reason CSS is hard to maintain, but the truth is you’re
+          never going to believe me until you actually try it. If you can
+          suppress the urge to retch long enough to give it a chance, I really
+          think you’ll wonder how you ever worked with CSS any other way.
+        </p>
+        <div className="flex w-full lg:space-x-5 flex-col lg:flex-row items-center justify-center space-y-3">
+          <div className="lg:w-1/3 bg-slate-100 rounded-lg  space-y-3 flex flex-col justify-center items-center border border-slate-300 p-6">
+            <Image
+              src="/person1.jpg"
+              alt=""
+              width={70}
+              height={70}
+              className="rounded-full -mt-12"
+            />
+            <h3 className="font-bold">Adam Sandler</h3>
+            <p className="text-sm text-center">
+              The way I wrote it changed frequently. It’s not a coincidence
+              Tailwind was released the same year. It might look wrong, but
+              spend time with it and you’ll realize semantic CSS was a 20 year
+              mistake.
+            </p>
+          </div>
+          <div className="lg:w-1/3 bg-slate-100 rounded-lg  space-y-3 flex flex-col justify-center items-center border border-slate-300 p-6">
+            <Image
+              src="/person1.jpg"
+              alt=""
+              width={70}
+              height={70}
+              className="rounded-full -mt-12"
+            />
+            <h3 className="font-bold">Adam Sandler</h3>
+            <p className="text-sm text-center">
+              The way I wrote it changed frequently. It’s not a coincidence
+              Tailwind was released the same year. It might look wrong, but
+              spend time with it and you’ll realize semantic CSS was a 20 year
+              mistake.
+            </p>
+          </div>
+          <div className="lg:w-1/3 bg-slate-100 rounded-lg  space-y-3 flex flex-col justify-center items-center border border-slate-300 p-6">
+            <Image
+              src="/person1.jpg"
+              alt=""
+              width={70}
+              height={70}
+              className="rounded-full -mt-12"
+            />
+            <h3 className="font-bold">Adam Sandler</h3>
+            <p className="text-sm text-center">
+              The way I wrote it changed frequently. It’s not a coincidence
+              Tailwind was released the same year. It might look wrong, but
+              spend time with it and you’ll realize semantic CSS was a 20 year
+              mistake.
+            </p>
+          </div>
+        </div>
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="bg-orange-500">
+        <div className="flex lg:justify-between container lg:mx-auto px-6 py-14 flex-col lg:flex-row mx-auto lg:space-y-0 space-y-5 justify-center items-center">
+          <h1 className="lg:text-4xl text-3xl text-center lg:text-start text-white font-bold">
+            Move even faster with Tailwind UI.
+          </h1>
+          <button className="bg-white text-orange-500 rounded-full lg:px-7 lg:py-4 px-7 py-4 hover:bg-slate-800 transition-all ease-in-out">
+            Start Here
+          </button>
+        </div>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className="bg-slate-900">
+        <div className="flex flex-col lg:flex-row  bg-slate-400-500 container mx-auto px-6 py-20">
+          <div className="flex-1 flex flex-col space-y-9 self-center lg:self-none  ">
+            <h3 className="text-white order-2 lg:order-none mt-5 lg:mt-0">
+              Copyright © 2023, All Rights Reserved
+            </h3>
+            <Image src="/logo-dark.png" alt="" width={300} height={100} />
+            <div className="flex lg:space-x-4 self-center space-x-5 lg:self-start">
+              <span>
+                <RiFacebookBoxFill className="text-white w-8 h-8" />
+              </span>
+              <span>
+                <BsInstagram className="text-white w-8 h-8" />
+              </span>
+              <span>
+                <FaYoutube className="text-white w-8 h-8" />
+              </span>
+              <span>
+                <BsLinkedin className="text-white w-8 h-8" />
+              </span>
+              <span>
+                <FaTwitter className="text-white w-8 h-8" />
+              </span>
+            </div>
+          </div>
+          <div className="flex-1  justify-around lg:flex hidden">
+            <div className="flex flex-col space-y-4">
+              <h3 className="font-bold text-white">Home</h3>
+              <h3 className="font-bold text-white">about</h3>
+              <h3 className="font-bold text-white">Contact</h3>
+            </div>
+            <div className="flex flex-col space-y-4">
+              <h3 className="font-bold text-white">Important</h3>
+              <h3 className="font-bold text-white">Privacy</h3>
+              <h3 className="font-bold text-white">Credits</h3>
+            </div>
+          </div>
+          <div className="flex-1  flex-col items-end justify-between lg:flex hidden">
+            <div className="flex space-x-5">
+              <input type="text" placeholder="Subscribe to NewsLetter" className="text-slate-300 px-7 py-2 rounded-2xl" />
+              <button className="bg-orange-500 text-white px-7 py-2 rounded-full">Go</button>
+            </div>
+            <div>
+              <h3 className="text-white">Copyright © 2023, All Rights Reserved</h3>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
